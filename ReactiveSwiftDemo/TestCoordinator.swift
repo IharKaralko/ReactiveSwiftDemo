bf06d55329b5e8d6166294e410e0f376826ba939
Ihar_Karalko <korolkoigor697@gmail.com>
2018-02-19 11:38:14 +0300

    work with reactive TestCoordinator

---
+++ ReactiveSwiftDemo/TestCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  TestCoordinator.swift
+//  ReactiveSwiftDemo
+//
+//  Created by Ihar_Karalko on 17.02.2018.
+//  Copyright © 2018 Ihar_Karalko. All rights reserved.
+//
+
+import Foundation
---
+++ ReactiveSwiftDemo/TestViewModelProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  TestViewModelProtocol.swift
+//  ReactiveSwiftDemo
+//
+//  Created by Ihar_Karalko on 15.02.2018.
+//  Copyright © 2018 Ihar_Karalko. All rights reserved.
+//
+
+import Foundation
